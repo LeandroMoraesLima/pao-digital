c667063aef8828b7d17d745df50e7c23f6d09770
leandro <le.mlima@hotmail.com>
2018-08-31 00:34:15 -0300

    foi feito parceiros

--- wp-content/themes/PaoDigital/index.php
+++ wp-content/themes/PaoDigital/index.php
@@ -247,51 +247,59 @@ Our Team Section
 <!--==========================
 More Features Section
 ============================-->
-<section id="more-features" class="section-bg">
+<section id="more-features">
 	<div class="container">
-
 		<div class="section-header">
-			<h3 class="section-title">More Features</h3>
+			<h3 class="section-title">Parceiros</h3>
 			<span class="section-divider"></span>
 			<p class="section-description">Encontre alguns de nossos parceiros pela cidade</p>
 		</div>
-
-		<div class="row">
-
-			<div class="col-lg-6">
-				<div class="box wow fadeInLeft">
-					<div class="icon"><i class="ion-ios-stopwatch-outline"></i></div>
-					<h4 class="title"><a href="">Parceiros</a></h4>
-					<p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident etiro rabeta lingo.</p>
-				</div>
-			</div> 
-
-			<div class="col-lg-6">
-				<div class="box wow fadeInRight">
-					<div class="icon"><i class="ion-ios-bookmarks-outline"></i></div>
-					<h4 class="title"><a href="">Dolor Sitema</a></h4>
-					<p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
-				</div>
+		<div class="row wow fadeInUp">
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
 			</div>
-
-			<div class="col-lg-6">
-				<div class="box wow fadeInLeft">
-					<div class="icon"><i class="ion-ios-heart-outline"></i></div>
-					<h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-					<p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur trinige zareta lobur trade.</p>
-				</div>
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
 			</div>
-
-			<div class="col-lg-6">
-				<div class="box wow fadeInRight">
-					<div class="icon"><i class="ion-ios-analytics-outline"></i></div>
-					<h4 class="title"><a href="">Magni Dolores</a></h4>
-					<p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum rideta zanox satirente madera</p>
-				</div>
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
 			</div>
-
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
+			</div>
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
+			</div>
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
+			</div>
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
+			</div>
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
+			</div>
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
+			</div>
+			<div class="col-xs-6 col-sm-3 col-md-15 col-lg-15">  
+				<div style="background-image: url('<?php echo IMG . '/logo-primicia.png'; ?>');"></div>  
+				<h4>Aclimação-SP</h4>									
+			</div>
 		</div>
-	</div>
+		<div class="botao">
+			<a href="#" class="ver">Ver todos parceiros</a>
+		</div>	
+	</div>	
 </section><!-- #more-features -->
 
 <!--==========================
