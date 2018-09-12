3255e0c2d7c2c7b152f87bff89744af2a70b5537
leandro <le.mlima@hotmail.com>
2018-09-12 01:41:20 -0300

    feito o painel

---
+++ wp-content/themes/PaoDigital/lib/template-vendas.php
@@ -0,0 +1,71 @@
+<div class="inside">
+	<table class="table card-table">
+		<thead>
+			<tr>
+				<th>#ID</th>
+				<th style="width:40%">Cliente</th>
+				<th>Valor</th>
+				<th>Item</th>
+				<th>Entrega</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td class="align-middle" style="width: 40px">
+					#00001	
+				</td>
+				<td class="align-middle">
+					<a href="javascript:void(0)" class="text-dark">Bolota</a>
+				</td>
+				<td class="align-middle">$480.00</td>
+				<td class="align-middle">123</td>
+				<td class="align-middle">7:00</td>
+			</tr>
+			<tr>
+				<td class="align-middle">
+					#00002
+				</td>
+				<td class="align-middle">
+					<a href="javascript:void(0)" class="text-dark">Bolota</a>
+				</td>
+				<td class="align-middle">$57.55</td>
+				<td class="align-middle">222</td>
+				<td class="align-middle">8:10</td>
+			</tr>
+			<tr>
+				<td class="align-middle">
+					#00003
+				</td>
+				<td class="align-middle">
+					<a href="javascript:void(0)" class="text-dark">Bolota</a>
+				</td>
+				<td class="align-middle">$235.55</td>
+				<td class="align-middle">43</td>
+				<td class="align-middle">9:35</td>
+			</tr>
+			<tr>
+				<td class="align-middle">
+					#00004
+				</td>
+				<td class="align-middle">
+					<a href="javascript:void(0)" class="text-dark">Bolota</a>
+				</td>
+				<td class="align-middle">$160.00</td>
+				<td class="align-middle">38</td>
+				<td class="align-middle">10:00</td>
+			</tr>
+			<tr>
+				<td class="align-middle">
+					#00005
+				</td>
+				<td class="align-middle">
+					<a href="javascript:void(0)" class="text-dark">Bolota</a>
+				</td>
+				<td class="align-middle">$159.00</td>
+				<td class="align-middle">34</td>
+				<td class="align-middle">10:45</td>
+			</tr>
+		</tbody>
+	</table>
+	<a href="javascript:void(0)" class="card-footer d-block text-center text-dark small font-weight-semibold">SHOW MORE</a>
+</div>
