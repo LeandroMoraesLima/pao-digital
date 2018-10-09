dcab8b317aa4050a101155509ff4cbafd639a27c
Alex Sandro <alex@quicktech.no>
2018-10-09 18:54:50 -0300

    adjusts paginas

---
+++ wp-content/themes/PaoDigital/page.php
@@ -0,0 +1,44 @@
+<?php 
+/*
+	template name: Contato
+*/
+
+get_header('interna');
+
+if (have_posts()) :
+    while (have_posts()) : the_post();
+
+?>
+
+
+<!--==========================
+Our Team Section
+============================-->
+<section id="team" class="interna">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="container">
+				<div class="row">
+					
+					<div class="text col-lg-12 content wow fadeInRight">
+						<h3 class="title"><?php the_title(); ?></h3>
+						<hr class="line">
+						<span class="section-divider"></span>
+						<p class="section-description">
+							<?php the_content(); ?> 
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section><!-- #team -->
+
+
+
+<?php 
+	endwhile;
+endif;
+?>
+
+<?php get_footer(); ?>
